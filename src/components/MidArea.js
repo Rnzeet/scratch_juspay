44004ae36a74bb0d816b4480e37819558411a8d4
Ranjit-nineleaps <ranjit.mahato@nineleaps.com>
2023-06-22 18:59:33 +0530

    juspay assignemtnt

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 export default function MidArea() {
   const [droppedItems, setDroppedItems] = useState([]);
@@ -13,12 +14,18 @@ export default function MidArea() {
     event.preventDefault();
   };
   return (
-    <div className="flex-1 h-full  overflow-auto" onDrop={handleDrop} onDragOver={handleDragOver}>
+    <div style={{ clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)' }} className="flex-1 h-full  overflow-auto" onDrop={handleDrop} onDragOver={handleDragOver}>
       <ul>
-        {droppedItems.map((item,index) => (
-          <div  className="flex bg-yellow-500 text-white place-content-center mx-72  mt-10 py-2 text-sm cursor-pointer" key={index}>{item.text}</div>
+        {droppedItems.map((item, index) => (
+          <div>
+            <Draggable>
+              <div className="flex bg-yellow-500 text-white place-content-center mx-72  mt-10 py-2 text-sm cursor-pointer" key={index}>
+                {item.text}
+              </div>
+            </Draggable>
+          </div>
         ))}
-        </ul>
+      </ul>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -32,7 +32,7 @@ function Sidebar(props) {
   return (
     <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
       <div className="font-bold"> {"Events"} </div>
-      <div draggable={true}
+      <div draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 55% 15%, 50% 0%, 100% 0%, 100% 75%, 60% 75%,48% 100%,30% 100%, 20% 74%, 0% 74%)'}}
         onDragStart={(event) => handleDragStart(event, "When  clicked")}>
         <div className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer"
         >
@@ -41,41 +41,41 @@ function Sidebar(props) {
           {"clicked"}
         </div>
       </div>
-      <div draggable={true}
-        onDragStart={(event) => handleDragStart(event, "When key Pressed")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+      <div draggable={true}style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "When key Pressed")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-3 my-2 text-sm cursor-pointer">
         {"When "}
         <Dropdown options={key_pressed} value={keyPressed} placeholder="select option" className="text-xs" />
         {"key pressed"}
       </div>
 
-      <div draggable={true}
+      <div draggable={true}style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
         onDragStart={(event) => handleDragStart(event, "When this sprite clicked")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
         {"When this sprite clicked"}
       </div>
-      <div draggable={true}
-        onDragStart={(event) => handleDragStart(event, "When backdrop switches to")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+      <div draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "When backdrop switches to")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-3 my-2 text-sm cursor-pointer">
         {"When backdrop switches to"}
         <Dropdown options={backdrop1} value={backDrop1} placeholder="select option" className="text-xs" />
       </div>
-      <div draggable={true}
-        onDragStart={(event) => handleDragStart(event, "When >10")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+      <div draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "When >10")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-4 my-1 text-sm cursor-pointer">
         {"When "}
         <Dropdown options={loudness} value={loudNess} placeholder="select option" className="text-xs" />
         {">"}
         <div className="mx-2 py-1 px-2 bg-white rounded-md text-black" >{"10"}</div>
       </div>
-      <div draggable={true}
-        onDragStart={(event) => handleDragStart(event, "When I receive")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+      <div draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "When I receive")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-3 my-1 text-sm cursor-pointer">
         {"When I receive"}
         <Dropdown options={message} value={messageBroad} placeholder="select option" className="text-xs" />
       </div>
-      <div draggable={true}
-        onDragStart={(event) => handleDragStart(event, "broadcast")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+      <div draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "broadcast")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-3 my-1 text-sm cursor-pointer">
         {"broadcast"}
         <Dropdown options={message} value={messageBroad} placeholder="select option" className="text-xs" />
       </div>
-      <div draggable={true}
-        onDragStart={(event) => handleDragStart(event, "broadcast")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+      <div draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "broadcast")} className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-4 my-2 text-sm cursor-pointer">
         {"broadcast"}
         <Dropdown options={message} value={messageBroad} placeholder="select option" className="text-xs" />
       </div>
@@ -85,25 +85,25 @@ function Sidebar(props) {
 
 
       <div className="font-bold"> {"Motion"} </div>
-      <button draggable={true}
-        onDragStart={(event) => handleDragStart(event, "Move 10 steps")} onClick={() =>{ handleClick(`ml-${a}`)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer after:content-['*'] after:bg-red-500 after:px-2 after:py-2 after:my-2 before:content-['*'] before:bg-red-500 before:px-1 before:py-1 before:my-1" >
+      <button draggable={true} style={{clipPath: 'polygon(0% 0%, 22% 18%, 5% 15%, 0% 0%, 100% 0%, 100% 92%, 50% 92%,40% 100%,20% 100%, 16% 92%, 0% 92%)'}}
+        onDragStart={(event) => handleDragStart(event, "Move 10 steps")} onClick={() =>{ handleClick(`ml-${a}`)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-3 my-2 text-sm cursor-pointer after:content-['*'] after:bg-red-500 after:px-2 after:py-2 after:my-2 before:content-['*'] before:bg-red-500 before:px-1 before:py-1 before:my-1" >
         {"Move "}
         <div className="mx-2 py-1 px-2 bg-white rounded-md text-black " >{"10"}</div>{"steps"}
       </button>
       <button draggable={true}
-        onDragStart={(event) => handleDragStart(event, "Turn 15 degrees")} onClick={() =>{ handleClick(` transform rotate-${b}`)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+        onDragStart={(event) => handleDragStart(event, "Turn 15 degrees")} onClick={() =>{ handleClick(` transform rotate-${b}`)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-3 my-2 text-sm cursor-pointer">
         {"Turn "}
         <Icon name="undo" size={15} className="text-white mx-2" />
         <div className="mx-2 py-1 px-2 bg-white rounded-md text-black" >{"15"}</div> {"degrees"}
       </button>
       <button draggable={true}
-        onDragStart={(event) => handleDragStart(event, "Turn 15 degrees")}  onClick={() =>{ handleClick(`transform rotate-${c} `)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+        onDragStart={(event) => handleDragStart(event, "Turn 15 degrees")}  onClick={() =>{ handleClick(`transform rotate-${c} `)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-3 my-2 text-sm cursor-pointer">
         {"Turn "}
         <Icon name="redo" size={15} className="text-white mx-2" />
         <div className="mx-2 py-1 px-2 bg-white rounded-md text-black" >{"15"}</div> {"degrees"}
       </button>
       <button draggable={true}
-        onDragStart={(event) => handleDragStart(event, "go to Select option")} onClick={() =>{ handleClick(`mx-${Math.floor(Math.random())} my-${Math.floor(Math.random())}`)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
+        onDragStart={(event) => handleDragStart(event, "go to Select option")} onClick={() =>{ handleClick(`mx-${Math.floor(Math.random())} my-${Math.floor(Math.random())}`)}} className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-3 my-2 text-sm cursor-pointer">
         {"go to"}
         <Dropdown options={randomPosition} value={randomPos} placeholder="select option" className="text-xs" />
       </button>
